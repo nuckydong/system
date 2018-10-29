3dd4e382d1af1874a1338c1a5415b33a662630fb
dongyy@geesafe.com <nuckydong@gmail.com>
2018-10-29 16:32:21 +0800

    删除多余字段

--- src/main/java/com/gopher/system/model/vo/response/LoginResponse.java
+++ src/main/java/com/gopher/system/model/vo/response/LoginResponse.java
@@ -1,27 +1,13 @@
 package com.gopher.system.model.vo.response;
 
 public class LoginResponse {
-	private int userId;
 	private String token;
-	private long expiry;
 	
 	public String getToken() {
 		return token;
 	}
 	public void setToken(String token) {
 		this.token = token;
-	}
-	public long getExpiry() {
-		return expiry;
-	}
-	public void setExpiry(long expiry) {
-		this.expiry = expiry;
-	}
-	public int getUserId() {
-		return userId;
-	}
-	public void setUserId(int userId) {
-		this.userId = userId;
 	}
 	
 }
